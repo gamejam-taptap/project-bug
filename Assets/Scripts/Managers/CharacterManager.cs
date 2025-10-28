aef73e9129d74e3414151740e04ad81b086be4f6
isolamento <isolamenter@gmail.com>
2025-10-28 19:16:28 +0800

    动画调整

--- Assets/Scripts/Managers/CharacterManager.cs
+++ Assets/Scripts/Managers/CharacterManager.cs
@@ -5,9 +5,11 @@ public class CharacterManager : BaseManager<CharacterManager>
     public GameObject player;
     public float moveSpeed = 5f;          // 移动速度
     public Rigidbody2D rb;
+    public Animator animator;
     
     private Vector2 moveInput;            // 输入方向
     private bool _isPlayerVisible;
+    private const float Scale = 8f;
     
     private void Awake()
     {
@@ -27,12 +29,29 @@ public class CharacterManager : BaseManager<CharacterManager>
         float moveY = Input.GetAxisRaw("Vertical");   // W/S 或 ↑ ↓
 
         moveInput = new Vector2(moveX, moveY).normalized; // 归一化避免斜向移动过快
+
+        if (moveX > 0)
+        {
+            player.transform.localScale = new Vector3(-Scale, Scale, Scale);
+        }
+        else if (moveX < 0)
+        {
+            player.transform.localScale = new Vector3(Scale, Scale, Scale);
+        }
     }
 
     void FixedUpdate()
     {
         // 使用 Rigidbody2D 移动
         rb.linearVelocity = moveInput * moveSpeed;
+        if (moveInput.magnitude > 0)
+        {
+            animator.Play("walk");
+        }
+        else
+        {
+            animator.Play("idle");       
+        }
     }
 
     public void ShowPlayer()
